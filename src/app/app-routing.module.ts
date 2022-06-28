f33c4b1bbd94dd1b283e1c604ce85752e60138bb
Ibrahim Bashir <ibrahimembash@gmail.com>
2022-06-28 20:29:13 +0200

    added Responsive Navbar and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventContainerComponent, EventDetailsComponent } from './pages';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: EventContainerComponent
+  },
+  {
+    path: 'event:id',
+    component: EventDetailsComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { EventContainerComponent } from './pages/event-container/event-container.component';
+import { EventDetailsComponent } from './pages/event-details/event-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    EventContainerComponent,
+    EventDetailsComponent
   ],
   imports: [
     BrowserModule,
