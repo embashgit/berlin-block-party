d4b57995507d2766bd0212ad86c9e6f016022d64
Ibrahim Bashir <ibrahimembash@gmail.com>
2022-06-29 02:16:11 +0200

    update yml

---
+++ src/app/models/events.model.ts
@@ -0,0 +1,11 @@
+export interface Events {
+    title:string;
+    link:string;
+    description: string;
+    pubDate:string;
+    creator: string;
+    guid: {
+        isPermaLink: string,
+        text: string
+    }
+}
--- src/app/service/event.service.ts
+++ src/app/service/event.service.ts
@@ -7,3 +7,6 @@ export class EventService {
 
   constructor() { }
 }
+
+
+  
