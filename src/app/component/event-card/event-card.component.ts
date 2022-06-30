936a10dc7a3845a646af089e56dc02d9207b6269
Ibrahim Bashir <ibrahimembash@gmail.com>
2022-06-30 20:44:02 +0200

    clean up lints

--- src/app/component/event-card/event-card.component.ts
+++ src/app/component/event-card/event-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IEventFeedItems, } from 'src/app/models/events.model';
 import { Clipboard } from '@angular/cdk/clipboard';
 @Component({
@@ -6,13 +6,11 @@ import { Clipboard } from '@angular/cdk/clipboard';
   templateUrl: './event-card.component.html',
   styleUrls: ['./event-card.component.css']
 })
-export class EventCardComponent implements OnInit {
+export class EventCardComponent  {
   @Input()
   EventItem!: IEventFeedItems;
   constructor(private clipboard: Clipboard) {}
 
-  ngOnInit(): void {
-  }
 
   public copyToClipboard(text:string):void{
     this.clipboard.copy(text)
