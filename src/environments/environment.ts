43f4089b5e3ebe30fba002289c2cdf9f81a74ae5
Ibrahim Bashir <ibrahimembash@gmail.com>
2022-07-04 16:42:49 +0200

    clear lints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,13 +8,4 @@ export const environment = {
   CORSURL:"https://bash-cors-any.herokuapp.com/",
   BASEURL:"https://daten.berlin.de/datensaetze/rss.xml/berliner-stra%C3%9Fen-und-volksfeste-2020",
   SEARCHURL:'https://www.berlin.de/sen/finanzen/service/zuwendungsdatenbank/index.php/index/index.json'
-};
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+}
